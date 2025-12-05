03998fc03f2458df58c8862bb1f297ff297785a8
León Radziunas <leon.radziunas@gmail.com>
2025-12-05 09:07:20 +0100

    Github action example

--- src/main.py
+++ src/main.py
@@ -2,3 +2,6 @@
 def elevar(num, exponente):
     return num ** exponente
 
+def sumar(num1, num2):
+    return num1 + exponente2
+
--- test/test_main.py
+++ test/test_main.py
@@ -1,3 +1,6 @@
-from src.main import elevar
+from src.main import elevar, sumar
 def test_elevar():
     assert elevar(2,3) == 8
+
+def test_sumar():
+    assert sumar(2,5) == 7
